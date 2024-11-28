1f535fba7077eb940958f7fa92ef5bc1337e44b6
Lukas Vogel <lukas@cedardb.com>
2024-11-28 10:06:02 +0100

    simplify queries in the nasdaq example
    - we can drop a subquery from the candlesticks query
    - make the order book depth query a little bit more readable

--- nasdaq/sql/candlesticks.sql
+++ nasdaq/sql/candlesticks.sql
@@ -4,37 +4,41 @@ with limits as (
            34200000000000 + (30*60)::bigint * 1000 * 1000 * 1000 as end, -- to 30*60 seconds = 30 minutes after market start.
            10::bigint * 1000 * 1000 * 1000 as step  -- bin size of 10 seconds
 ), bins as (
+    -- we always generate the bins from the start of the trading day so they are stable
     select generate_series(l.start ,l.end, l.step) as time
-    from limits l -- we always generate the bins from the start of the trading day so they are stable
+    from limits l 
 ),
-     prices as (
-         select e.timestamp as time, s.name as metric, max(coalesce(e.price, o.price)) as value, max(coalesce(e.price, o.price) * e.quantity)  as volume
-         from executions e, stocks s, orders o
-         where e.orderid = o.orderid
-           and o.stockid = s.stockid
-           and s.name in ('AAPL', 'MSFT') -- stock ticker symbols you're interested in
-         group by e.timestamp, s.name
-     ),
-     binned as (
-         select
-             extract(epoch from current_date + (b.time/(1000*1000) * interval '1 millisecond')) as time, -- time as if the stock data was from today
-             p.metric,
-             first_value(p.value) over w as open,
-             last_value(p.value) over w as close,
-             max(p.value) over w as high,
-             min(p.value) over w as low,
-             sum(p.volume) over w as volume,
-             row_number() over w as rn
-         from prices p, bins b, limits l
-         where p.time >= b.time and p.time < b.time + l.step
-           and p.time >= l.start
-           and p.time < l.end
-         window w as (partition by b.time, p.metric order by p.time asc),
-     )
-select metric as stock, time, open, close, high, low, volume from binned b
-where not exists (
-    select * from binned b2
-    where b2.metric = b.metric
-      and b2.time = b.time
-      and b2.rn > b.rn
-) order by b.metric, b.time asc;
+prices as (
+  -- for any order of a given stock executed within the relevant time span, find the price
+  select 
+    e.timestamp as time, s.name as metric, 
+    -- if the execution itself has a price, it takes precedence
+    max(coalesce(e.price, o.price)) as value,
+    max(coalesce(e.price, o.price) * e.quantity) as volume
+  from executions e, stocks s, orders o, limits l
+  where e.orderid = o.orderid
+    and o.stockid = s.stockid 
+    and s.name in ('AAPL', 'MSFT') -- stock ticker symbols we're interested in
+    and e.timestamp >= l.start
+    and e.timestamp < l.end
+  group by e.timestamp, s.name
+),
+binned as (
+  select 
+      extract(epoch from current_date + (b.time/(1000*1000) * interval '1 millisecond')) as time,
+      p.metric, 
+      first_value(p.value) over w as open,
+      last_value(p.value) over w as close,
+      max(p.value) over w as high,
+      min(p.value) over w as low,
+      sum(p.volume) over w as volume,
+      row_number() over w as rn
+  from prices p, bins b, limits l
+  -- assign each event into its bin
+  where p.time >= b.time and p.time < b.time + l.step
+  -- for each bin, find the candle stick parameters with a window function
+  window w as (partition by b.time, p.metric order by p.time asc rows between unbounded preceding and unbounded following)
+)
+select metric, time, open, close, high, low, volume 
+from binned b 
+where rn = 1;
--- nasdaq/sql/orderbookDepth.sql
+++ nasdaq/sql/orderbookDepth.sql
@@ -1,26 +1,27 @@
 -- Query data for an orderbook depth chart (trend line in Grafana)
-with orderdepth as (
+with orderdepth as ( 
+    -- count the number of available stock at each price for the buy and for the sell side
     select price, side, sum(quantity) as quantity
     from orderbook o, stocks s
     where o.stockId = s.stockId
-      and s.name = 'AAPL' -- stock ticker symbol you're interested in
+    and s.name = 'AAPL' -- stock ticker symbol we're interested in
     group by price, side
+    having sum(quantity) > 0
 ),
-     cumulative_sell as (
-         select price, side as metric, sum(quantity) over (order by price asc) as sum
-         from orderdepth
-         where quantity > 0
-           and side = 'SELL'
-     ),
-     cumulative_buy as (
-         select price, side as metric, sum(quantity) over (order by price desc) as sum
-         from orderdepth
-         where quantity > 0
-           and side = 'BUY'
-     ),
-     cumulative as (
-         select * from cumulative_sell where sum > 0 and price < 1.05 * (select min(price) from cumulative_sell) -- 1.05 is the width: Show all orders between 1/1.05x and 1.05x of the last trade
-         union all
-         select * from cumulative_buy where sum > 0 and price > 1::numeric/1.05 * (select max(price) from cumulative_buy) -- width again
-     )
-select * from cumulative order by price asc;
+cumulative_sell as (
+    -- transform the sell side into a cumulative sum with a window query
+    select price, side, sum(quantity) over (order by price asc) as sum
+    from orderdepth
+    where side = 'SELL'
+),
+cumulative_buy as (
+    -- transform the buy side into a cumulative sum with a window query
+    select price, side, sum(quantity) over (order by price desc) as sum
+    from orderdepth
+    where side = 'BUY'
+
+)
+-- stitch together buy and sell side
+select * 
+from (select * from cumulative_sell union all select * from cumulative_buy) 
+order by price;
